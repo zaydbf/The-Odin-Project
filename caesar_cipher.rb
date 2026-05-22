e09e298646811f41ed1178997e6afb59271be6ed
zaydbf <zaydbenfathel@gmail.com>
2026-05-22 01:58:40 +0100

    caesar-cipher solution

---
+++ caesar_cipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(text, shift)
+  encrypted = ""
+  if text.length != 0
+    text.each_char do |letter|
+      if letter.between?("a", "z")
+        new_letter = (((letter.ord - 97 + shift) % 26) + 97).chr
+        encrypted += new_letter    
+      elsif letter.between?("A", "Z")
+        new_letter = (((letter.ord - 65 + shift) % 26) + 65).chr
+        encrypted += new_letter
+      else
+        encrypted += letter             
+      end
+    end
+  end
+  return encrypted
+end
+
+puts caesar_cipher("What a string!", 5)
